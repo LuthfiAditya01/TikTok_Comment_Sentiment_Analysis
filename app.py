53b08c0ec2218006f84575fdfde7e35984952b86
LuthfiAditya01 <luthfi2004aditya@gmail.com>
2025-06-10 05:34:36 +0700

    Fix Download Button
    Fixing download button with using route from app.py

--- app.py
+++ app.py
@@ -8,22 +8,23 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/index')
-def index():
-    return render_template('index.html')
+# @app.route('/index')
+# def index():
+#     return render_template('index.html')
 
 @app.route('/download/dataset')
 def download_dataset():
     """Redirect to GitHub raw file for dataset download"""
     from flask import redirect
-    github_url = "https://github.com/LuthfiAditya01/TikTok_Comment_Sentiment_Analysis/raw/main/indonesia%20gelap.csv"
+    # Direct GitHub raw URL - browser will auto-download
+    github_url = "https://raw.githubusercontent.com/LuthfiAditya01/TikTok_Comment_Sentiment_Analysis/main/indonesia%20gelap.csv"
     return redirect(github_url)
 
 @app.route('/download/report')
 def download_report():
     """Redirect to GitHub raw file for report txt download"""
     from flask import redirect
-    github_url = "https://github.com/LuthfiAditya01/TikTok_Comment_Sentiment_Analysis/raw/main/model-report.zip"
+    github_url = "https://github.com/LuthfiAditya01/TikTok_Comment_Sentiment_Analysis/raw/main/static/model-report.zip"
     return redirect(github_url)
 
 # For Vercel deployment
